8553590b77726b571b95db0e1e0eee1429a8b89c
yashnuhash <nuhashroxme@gmail.com>
2023-06-05 00:57:25 +0600

    feat:Codded till Object-types

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var String = "Hello World!";
-console.log(String);
+var json = JSON.parse("60");
+console.log(typeof json);
+var Array = [];
+Array.push("NUHASH", "Polli");
+console.log(Array);
+var Array2 = ["ABCD"];
+console.log(Array2);
+var tuple;
+tuple = [90, true, "Spider-Monkey"];
+console.log(tuple);
+var car = {
+    type: "Tesla",
+    model: "Model-X",
+    year: 2017
+};
+console.log(car);
--- script.ts
+++ script.ts
@@ -1,5 +1,29 @@
-let String: string = "Hello World!";
+const json = JSON.parse("60");
+console.log(typeof json);
 
-console.log(String);
+const Array: string[] = [];
+Array.push("NUHASH","Polli");
+console.log(Array);
+
+const Array2: readonly string[] = ["ABCD"];
+console.log(Array2);
+
+let tuple: [number,boolean,string];
+tuple = [90,true,"Spider-Monkey"];
+console.log(tuple);
+
+const car: {
+    type: string,
+    model: string,
+    year: number
+} = {
+    type: "Tesla",
+    model: "Model-X",
+    year: 2017
+};
+
+car.type = "Ford";
+
+console.log(car);
 
 export{}
